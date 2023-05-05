a1215d59b0bb88959f0e2eda14aace3c85b05d8f
muhd zahir <81223199+mzramle@users.noreply.github.com>
2023-05-05 18:15:33 +0800

    Added the basic Flutter folder
    All of the Flutter files in these folders is in their basic state.

---
+++ flutter_pkuonline/README.md
@@ -0,0 +1,3 @@
+# flutter_pkuonline
+
+A new Flutter project.
---
+++ flutter_pkuonline/android/app/src/main/kotlin/com/example/flutter_pkuonline/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_pkuonline
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
